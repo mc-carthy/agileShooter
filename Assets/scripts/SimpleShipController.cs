e4c0371470106dfda0fdb80e58907ee5ca614437
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-28 22:39:56 +0000

    Add multi-platform move player method

--- Assets/scripts/SimpleShipController.cs
+++ Assets/scripts/SimpleShipController.cs
@@ -2,31 +2,47 @@
 
 public class SimpleShipController : MonoBehaviour {
 
-	private float tranSpeed = 5f;
-	private float rotSpeed = 50f;
+  private Vector2 delta = Vector2.zero;
+  private float tranSpeed = 5f;
+  private float rotSpeed = 50f;
 
-	private void Awake () {
+  private void Awake () {
 
-	}
+  }
 
-	private void Start () {
+  private void Start () {
 
-	}
+  }
 
-	private void Update () {
-		MoveShip();
-	}
+  private void Update () {
+    MoveShip();
+  }
 
-	private void MoveShip() {
-		float h = Input.GetAxisRaw("Horizontal");
-		float v = Input.GetAxisRaw("Vertical");
+  private void MoveShip() {
 
-		transform.Translate(
-			new Vector3(0, v, 0) * tranSpeed * Time.deltaTime
-		);
-		transform.Rotate(
-			new Vector3(0, 0, -h) * rotSpeed * Time.deltaTime
-		);
-	}
+    #if (UNITY_IOS || UNITY_ANDROID) && !UNITY_EDITOR || REMOTE
+
+    if (Input.touchCount > 0) {
+      Touch t = Input.touches[0];
+      if (t.phase == TouchPhase.Moved) {
+        delta.x = t.deltaPosition.x;
+        delta.y = t.deltaPosition.y;
+      }
+    }
+
+    #else
+
+    delta.x = Input.GetAxisRaw("Horizontal");
+    delta.y = Input.GetAxisRaw("Vertical");
+
+    #endif
+
+    transform.Translate(
+      new Vector3(0, delta.y, 0) * tranSpeed * Time.deltaTime
+    );
+    transform.Rotate(
+      new Vector3(0, 0, -delta.x) * rotSpeed * Time.deltaTime
+    );
+  }
 
 }
