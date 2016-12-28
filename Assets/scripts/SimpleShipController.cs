a2296c384dca70e94c3bae0a05dc842b88349939
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-28 22:33:39 +0000

    Undo clumsy reset...

---
+++ Assets/scripts/SimpleShipController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class SimpleShipController : MonoBehaviour {
+
+	private float tranSpeed = 5f;
+	private float rotSpeed = 50f;
+
+	private void Awake () {
+
+	}
+
+	private void Start () {
+
+	}
+
+	private void Update () {
+		MoveShip();
+	}
+
+	private void MoveShip() {
+		float h = Input.GetAxisRaw("Horizontal");
+		float v = Input.GetAxisRaw("Vertical");
+
+		transform.Translate(
+			new Vector3(0, v, 0) * tranSpeed * Time.deltaTime
+		);
+		transform.Rotate(
+			new Vector3(0, 0, -h) * rotSpeed * Time.deltaTime
+		);
+	}
+
+}
