4c823b717155dd03479950004f403da263cb11d5
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-29 18:23:41 +0000

    Spawning asteroids

---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu ("Vistage/Spawner")]
+public class Spawner : MonoBehaviour {
+
+	[HeaderAttribute ("Spawn")]
+	public GameObject reference;
+
+	[HeaderAttribute ("Spawning")]
+	[RangeAttribute (0.001f, 10f)]
+	public float rate = 1.0f;
+	public bool isInfinite;
+	public int number = 5;
+
+	private float _timeStamp;
+	private int _remaining;
+
+	private void Awake () 
+	{
+
+	}
+
+	private void Start () 
+	{
+		_timeStamp = Time.time;
+		_remaining = number;
+
+		StartCoroutine (Spawn ());
+	}
+
+	private void Update () 
+	{
+
+	}
+
+	private IEnumerator Spawn () {
+		while (isInfinite || _remaining > 0) {
+			Instantiate (reference, transform.position, Quaternion.identity);
+			_remaining--;
+			yield return new WaitForSeconds(1 / rate);
+		}
+	}
+}
