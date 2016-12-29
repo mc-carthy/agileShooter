e4c2099f3eedac1765f5faa3ad3cbe7144cf297f
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-29 17:52:31 +0000

    Integrating GameArea and FitAreaToCamera classes

--- Assets/scripts/GameArea.cs
+++ Assets/scripts/GameArea.cs
@@ -8,19 +8,35 @@
 [AddComponentMenu ("Vistage/GameArea")]
 public class GameArea : MonoBehaviour {
 
+	[SerializeField]
+	[HideInInspector]
 	private Rect _area;
 	public Rect Area {
-		get { return _area; }
-		set { _area = value; }
+		get { 
+			return _area; 
+		}
+		set { 
+			_area = value; 
+		}
 	}
 
 	public Vector2 size;
+	public Vector2 Size {
+		get {
+			return Area.size;
+		}
+		set {
+			size = value;
+			Area = new Rect (size.x * -0.5f, size.y * -0.5f, size.x, size.y);
+		}
+	}
+
 	public Color gizmoColor = new Color(0, 0, 1, 0.2f);
 	private Color gizmoWireColor;
 
-	// private void Awake () {
-	// 	SetArea(size);
-	// }
+	private void Awake () {
+		Size = size;
+	}
 
 	private void OnDrawGizmos () {
 		Gizmos.matrix = transform.localToWorldMatrix;
@@ -30,11 +46,8 @@ public class GameArea : MonoBehaviour {
 	}
 
 	private void OnValidate () {
-		SetArea(size);
+		Size = size;
 		gizmoWireColor = new Color (gizmoColor.r, gizmoColor.g, gizmoColor.b, 1);
 	}
 
-	public void SetArea (Vector2 size) {
-		Area = new Rect (size.x * -0.5f, size.y * -0.5f, size.x, size.y);
-	}
 }
