8654d082c9074b8ec1834ef741e082dfeae6ff30
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-29 18:54:47 +0000

    Ensure asteroids are spawned at a set distance from player

--- Assets/scripts/Spawner.cs
+++ Assets/scripts/Spawner.cs
@@ -17,27 +17,39 @@ public class Spawner : MonoBehaviour {
 
 	[HeaderAttribute ("Locations")]
 	public GameArea area;
+	public float minDistanceFromPlayer;
 
+	private Transform player;
 	private float _timeStamp;
 	private int _remaining;
 
-
-	private void Start () 
-	{
-		_timeStamp = Time.time;
+	private IEnumerator Start () {
 		_remaining = number;
+		if (minDistanceFromPlayer > 0)
+		{
+			GameObject playerGO = GameObject.FindGameObjectWithTag("Player");
+			if (playerGO != null)
+			{
+				player = playerGO.transform;
+			}
+			else 
+			{
+				Debug.LogWarning ("No Player found, assign 'Player' tag to Player object");
+			}
+		}
 
-		StartCoroutine (Spawn ());
-	}
-
-	private IEnumerator Spawn () {
 		while (isInfinite || _remaining > 0) {
 			Vector3 _position = (area != null) ? area.GetRandomPosition () : transform.position;
+			
+			if (player != null && Vector3.Distance (_position, player.transform.position) < minDistanceFromPlayer)
+			{
+				_position = (_position - player.position).normalized * minDistanceFromPlayer;
+			}
 			
 			Instantiate (reference, _position, Quaternion.identity);
 			_remaining--;
 			
-			yield return new WaitForSeconds(1 / Random.Range (minRate, maxRate));
+			yield return new WaitForSeconds (1 / Random.Range (minRate, maxRate));
 		}
 	}
 }
