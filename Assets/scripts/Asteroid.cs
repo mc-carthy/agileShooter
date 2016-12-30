de290012d70bea063baa61f6fab93829fc1288c3
Mick McCarthy <mccarthy.mick0012@gmail.com>
2016-12-30 23:20:10 +0000

    Implement scoring system

---
+++ Assets/scripts/Asteroid.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+[AddComponentMenu ("Vistage/Asteroid")]
+public class Asteroid : MonoBehaviour {
+
+	public int score = 5;
+
+	private void OnCollisionEnter2D (Collision2D other)
+	{
+		if (!other.gameObject.CompareTag("Projectile"))
+		{
+			return;
+		}
+
+		GameManager.Score += score;
+
+
+		// TODO : Use object pooling
+		Destroy (gameObject);
+	}
+}
--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -6,12 +6,13 @@ static public class GameManager {
 	public const float MAX_DAMAGE = 100;
 
 	static private float damage;
-	static public float Damage {
+	static public float Damage 
+	{
 		get {
 			return damage;
 		}
 		set {
-			if (damage != value) {
+			if (value != damage) {
 				damage = value;
 
 				if (damage >= MAX_DAMAGE)
@@ -24,12 +25,13 @@ static public class GameManager {
 	}
 
 	static private int lives = 5;
-	static public int Lives {
+	static public int Lives 
+	{
 		get {
 			return lives;
 		}
 		set {
-			if (lives != value) {
+			if (value != lives) {
 				lives = value;
 
 				if (lives <= 0)
@@ -39,4 +41,29 @@ static public class GameManager {
 			}
 		}
 	}
+
+	static private int score;
+	static public int Score
+	{
+		get { return score;}
+		set 
+		{
+			if (value != score) 
+			{
+				score = value;
+				if (score > HighScore)
+				{
+					HighScore = value;
+				}
+			}
+		}
+	}
+
+	static private int highScore;
+	static public int HighScore
+	{
+		get { return PlayerPrefs.GetInt ("HighScore"); }
+		set { PlayerPrefs.SetInt ("HighScore", value); }
+	}
+	
 }
